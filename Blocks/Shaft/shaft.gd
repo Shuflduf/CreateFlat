fd63e88f034a7d24f44d12b9208c57e8db3309bb
Shuflduf <shuflduf@shuflduf.xyz>
2025-08-22 14:21:43 -0600

    BASICALLY PERFECT GEARBOXES

--- Blocks/Shaft/shaft.gd
+++ Blocks/Shaft/shaft.gd
@@ -4,21 +4,15 @@ extends MechanicalComponent
 
 func _ready() -> void:
     super()
-    #$Bottom.sprites.material.set_shader_parameter(&"bottom_shown", true)
-    #$Bottom.sprites.material.set_shader_parameter(&"top_shown", false)
     $Bottom.rotated.connect(
         func():
             $Top.speed = -$Bottom.speed
-            #$Top.flipped = $Bottom.flipped
             $Top.sprites.frame = $Bottom.sprites.frame
             $Top.transfer_rotation()
-            #$Bottom.flipped = $Top.flipped
-            #$Bottom.sprites.frame = $Top.sprites.frame
     )
     $Top.rotated.connect(
         func():
             $Bottom.speed = -$Top.speed
-            #$Bottom.flipped = $Top.flipped
             $Bottom.sprites.frame = $Top.sprites.frame
             $Bottom.transfer_rotation()
     )
