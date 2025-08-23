c2233df8bcb26b8eec524ccbc8b727fbb9714d90
Shuflduf <shuflduf@gmail.com>
2025-08-23 16:30:30 -0600

    ANIMATIONS AND ITEMS GO THROUGH BLOCKS

--- Blocks/Ejector/ejector.gd
+++ Blocks/Ejector/ejector.gd
@@ -1,11 +1,11 @@
 extends ItemTransport
 
+var is_ready = true
 
 func _ready() -> void:
     super()
     $Area.body_entered.connect(_on_area_item_entered)
-    print(target_pos)
-    $Sprites.scale.x = (-1.0 if target_pos.x <= tile_pos.x else 1.0)
+    $Sprites.scale.x = (1.0 if target_pos.x <= tile_pos.x else -1.0)
 
 
 func _physics_process(delta: float) -> void:
@@ -19,10 +19,20 @@ func _physics_process(delta: float) -> void:
                 held_item.global_position.x, global_position.x, delta * 5.0
             )
             held_item.position.y = global_position.y - 80.0
-        else:
+        elif is_ready:
+            is_ready = false
             var difference = target_pos - tile_pos
+            # I PULLED THESE NUMBERS OUT OF MY ASS
             held_item.velocity.y = (difference.y * 64.0) - 1000.0
             held_item.velocity.x = difference.x * 64.0
+            held_item.flying = true
+            held_item.fly_destination = Vector2(target_pos * 128)
             held_item = null
+            $Anim.play(&"launch")
 
     super(delta)
+
+
+func _on_anim_animation_finished(anim_name: StringName) -> void:
+    if anim_name == &"launch":
+        is_ready = true
--- item.gd
+++ item.gd
@@ -1,16 +1,24 @@
 class_name Item
 extends CharacterBody2D
 
-var is_ready = false
+@onready var default_collision = collision_layer
 
+var is_ready = false
+var flying = false
+var fly_destination: Vector2
 
 func _physics_process(delta: float) -> void:
     velocity.y += get_gravity().y * delta
     move_and_slide()
+    if flying:
+        if fly_destination.distance_squared_to(position) > 4096.0:
+            collision_layer = 0
+        else:
+            flying = false
+            collision_layer = default_collision
 
 
 func temp_disable():
-    var current_col = collision_layer
     collision_layer = 0
     await get_tree().create_timer(0.5).timeout
-    collision_layer = current_col
+    collision_layer = default_collision
