dcfe3c351092387cc7eecebf368180621be03ae9
Shuflduf <shuflduf@shuflduf.xyz>
2025-08-27 16:55:37 -0600

    drills doe i think

--- Blocks/Drill/drill.gd
+++ Blocks/Drill/drill.gd
@@ -5,20 +5,28 @@ var speed = 0.0:
     set(value):
         speed = value
         $DrillPart.speed_scale = abs(speed)
+        $AnimationPlayer.speed_scale = abs(speed)
 var target_source: ItemSource
+
+func mine():
+    if target_source:
+        var new_item = Item.from_id(target_source.item_data.id)
+        new_item.position = target_source.global_position
 
 func _physics_process(_delta: float) -> void:
     if not active:
         return
     $BreakParticles.emitting = target_source != null and speed != 0.0
+    if target_source and not $AnimationPlayer.is_playing():
+        $AnimationPlayer.play(&"mine")
 
 
 func _ready():
     super()
     await get_tree().physics_frame
+    await get_tree().physics_frame
     if not active:
         return
-    await get_tree().physics_frame
 
     $Connector.rotated.connect(func(): speed = $Connector.speed)
     if $Area.has_overlapping_areas():
