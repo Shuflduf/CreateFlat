200b6130692eb4fd2b323b84fb2d7d32dff93690
Shuflduf <shuflduf@gmail.com>
2025-08-27 14:37:21 -0600

    actually decent vein gen

--- item_source_spawner.gd
+++ item_source_spawner.gd
@@ -4,8 +4,7 @@ extends Node2D
 
 @export_tool_button("Spawn") var create_sources_action = create_sources
 @export var item_source_scene: PackedScene
-@export var circle_radius = 5
-@export var max_offset_radius = 10
+@export var circle_radius = 5.0
 
 var current_blocks_pos: Array[Vector2i]
 
@@ -14,19 +13,13 @@ func create_sources():
     for child in get_children():
         child.queue_free()
 
-    var center_offset = Vector2i(
-        randi_range(-max_offset_radius, max_offset_radius),
-        randi_range(-max_offset_radius, max_offset_radius),
-    )
-    var top_left_offset = center_offset + Vector2i(circle_radius, circle_radius)
-    for x in circle_radius * 2:
-        for y in circle_radius * 2:
-            var pos = Vector2i(x, y) + center_offset
-            print(pos)
+    for x in circle_radius * 2.0:
+        for y in circle_radius * 2.0:
+            var pos = Vector2(x - circle_radius, y - circle_radius)
+            if pos.length() > circle_radius:
+                continue
+            if pos.length() > randf_range(circle_radius / 2.0, circle_radius):
+                continue
             var new_source = item_source_scene.instantiate()
             new_source.position = Vector2(pos) * 128.0
             add_child(new_source)
-
-
-
-    print("A")
