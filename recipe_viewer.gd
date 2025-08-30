0ec1aee9e6f7a6166d6790267394e885192904df
Shuflduf <shuflduf@shuflduf.xyz>
2025-08-29 18:01:07 -0600

    all of pressing

--- recipe_viewer.gd
+++ recipe_viewer.gd
@@ -3,6 +3,7 @@ extends Control
 @export var single_recipe: PackedScene
 
 func _ready():
-    var new_recipe = single_recipe.instantiate()
-    %RecipeList.add_child(new_recipe)
-    new_recipe.populate(RecipeSystem.recipes[RecipeSystem.RecipeType.PRESSING][0])
+    for recipe in RecipeSystem.recipes[RecipeSystem.RecipeType.PRESSING]:
+        var new_recipe = single_recipe.instantiate()
+        %RecipeList.add_child(new_recipe)
+        new_recipe.populate(recipe)
--- single_recipe.gd
+++ single_recipe.gd
@@ -12,10 +12,12 @@ func populate(recipe: ItemRecipe):
 func populate_item_set(items: Dictionary[String, int]):
     for item in items:
         var count = items[item]
-        var tex = RecipeSystem.all_item_data.filter(func(i: ItemData): return i.id == item)[0].texture
+        var item_data = RecipeSystem.all_item_data.filter(func(i: ItemData): return i.id == item)[0]
+        var tex = item_data.texture
         for i in count:
             var new_icon = %BaseTexture.duplicate()
             %BaseTexture.get_parent().add_child(new_icon)
+            new_icon.tooltip_text = item_data.name
             new_icon.texture = tex
             new_icon.show()
 
